05076f8eeaee75fc2275eeaf8e8690809858499b
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2024-04-02 14:17:44 +0530

    updating production code

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
 import userRoutes from './routes/userRoutes.js';
 import teamRoutes from './routes/teamRoutes.js';
-import connectDB from './config/db.js'; 
+import connectDB from './config/db.js';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
-import path from 'path'; // Import the 'path' module
-
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+// Determine the environment
+const environment = process.env.NODE_ENV || 'development';
+
+// Load environment-specific configurations
+if (environment === 'development') {
+    dotenv.config({ path: '.env.development' });
+} else if (environment === 'production') {
+    dotenv.config({ path: '.env.production' });
+}
 
 const port = process.env.PORT || 8000;
 
@@ -22,10 +32,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from build directory in production
+if (environment === 'production') {
+    const buildPath = path.join(__dirname, '../frontend-r/build');
+    app.use(express.static(buildPath));
+}
 
-const buildPath = path.join(__dirname, '../frontend-r/build');
-app.use(express.static(buildPath));
-
+// Define routes
 app.use('/api/users', userRoutes);
 app.use('/api/teams', teamRoutes);
 
@@ -34,5 +47,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port} in ${environment} mode`);
 });
